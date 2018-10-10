0466c02252884d3856ac6e7a8ec2b731d85f25a7
Jeremy Bruns <bruns.jeremy@gmail.com>
2018-10-10 11:30:16 +0200

    adding rules to the laravel validator

--- src/PasswordValidationServiceProvider.php
+++ src/PasswordValidationServiceProvider.php
@@ -3,6 +3,8 @@
 namespace J3rrey\PasswordValidation;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use J3rrey\PasswordValidation\Rules;
 
 class PasswordValidationServiceProvider extends ServiceProvider
 {
@@ -14,11 +16,25 @@ class PasswordValidationServiceProvider extends ServiceProvider
     public function boot()
     {
 
-    //    $this->loadRulesFrom(__DIR__.'/Rules');
-
         $this->publishes([
             __DIR__.'/Rules' => app_path('/Rules'),
         ]);
+
+        Validator::extend('uppercase', function ($attribute, $value, $parameters, $validator) {
+            return new Rules\Uppercase();
+        });
+
+        Validator::extend('lowercase', function ($attribute, $value, $parameters, $validator) {
+            return new Rules\Lowercase();
+        });
+
+        Validator::extend('digit', function ($attribute, $value, $parameters, $validator) {
+            return  new Rules\Digit();
+        });
+
+        Validator::extend('standard', function ($attribute, $value, $parameters, $validator) {
+            return  new Rules\Digit();
+        });
     }
 
     /**
---
+++ src/Rules/BaseRule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerry
+ * Date: 02.10.18
+ * Time: 20:05
+ */
+
+namespace J3rrey\PasswordValidation\Rules;
+
+
+class BaseRule
+{
+
+}
